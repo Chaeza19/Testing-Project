fbfd0ce31f1dec9854dbe16575e434336d4ce6b7
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-03-03 23:57:21 +0700

    incremental development.

--- app/src/main/java/org/d3if3056/testing/MainActivity.kt
+++ app/src/main/java/org/d3if3056/testing/MainActivity.kt
@@ -1,40 +1,87 @@
 package org.d3if3056.testing
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3056.testing.R
+import org.d3if3056.testing.model.Hewan
 import org.d3if3056.testing.ui.theme.TestingTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val data = getData()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            TestingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Ayam", R.drawable.ayam),
+            Hewan("Bebek", R.drawable.bebek),
+            Hewan("Domba", R.drawable.domba),
+            Hewan("Kambing", R.drawable.kambing),
+            Hewan("Sapi", R.drawable.sapi),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(content: @Composable (Modifier) -> Unit) {
+    Scaffold(
+        topBar = { //Untuk Bagian TopBar
+            TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
     }
 }
 
 @Composable
-fun HitungButton(onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(text = "Hitung + 1")
+fun Greeting(name: String) {
+    MainScreen {modifier ->
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
 }
 
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true) // Untuk Dark Mode (uiMode)
 @Composable
-fun MainScreen() {
-    var hitung = 0
-    Column {
-        HitungButton { Column
-            hitung++
-        }
-        Text(text = "Hasil hitungan: $hitung")
+fun GreetingPreview() {
+    TestingTheme {
+        Greeting("Android")
     }
 }
