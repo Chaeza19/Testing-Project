11a7fc953b1764e6efb06ce4c7b066083bafc44e
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-03-08 22:14:21 +0700

    Memberi pilihan kepada pengguna

--- app/src/main/java/org/d3if3056/testing/MainActivity.kt
+++ app/src/main/java/org/d3if3056/testing/MainActivity.kt
@@ -1,22 +1,12 @@
 package org.d3if3056.testing
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3056.testing.ui.screen.MainScreen
 import org.d3if3056.testing.ui.theme.TestingTheme
 
@@ -34,14 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true) // Untuk Dark Mode (uiMode)
-@Composable
-fun GreetingPreview() {
-    TestingTheme {
-//        Greeting("Android")
     }
 }
