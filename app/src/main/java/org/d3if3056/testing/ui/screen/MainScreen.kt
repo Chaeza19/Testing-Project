b34990eecfa83a85e712405a586a47961e1d016c
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-03-25 00:13:37 +0700

    Membuat item dapat diklik

--- app/src/main/java/org/d3if3056/testing/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3056/testing/ui/screen/MainScreen.kt
@@ -1,6 +1,8 @@
 package org.d3if3056.testing.ui.screen
 
 import android.content.res.Configuration
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +20,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -54,6 +57,7 @@ fun ScreenContent(modifier: Modifier){
     val viewModel: MainViewModel = viewModel()
     val data = viewModel.data
 //    val data = emptyList<Catatan>()
+    val context = LocalContext.current
 
     if (data.isEmpty()){
         Column(
@@ -71,7 +75,10 @@ fun ScreenContent(modifier: Modifier){
             modifier = modifier.fillMaxWidth()
         ){
             items(data){
-                ListItem(catatan = it)
+                ListItem(catatan = it){
+                    val pesan = context.getString(R.string.x_diklik, it.judul)
+                    Toast.makeText(context, pesan, Toast.LENGTH_SHORT).show()
+                }
                 Divider()
             }
         }
@@ -79,10 +86,11 @@ fun ScreenContent(modifier: Modifier){
 }
 
 @Composable
-fun ListItem(catatan: Catatan){
+fun ListItem(catatan: Catatan, onClick: () -> Unit){
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .clickable { onClick() }
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
