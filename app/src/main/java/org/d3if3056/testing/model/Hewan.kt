db9ac6df0f7717d1adc13193b27d8690b684b319
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-05-11 22:18:38 +0700

    Memproses data string sebagai JSON

--- app/src/main/java/org/d3if3056/testing/model/Hewan.kt
+++ app/src/main/java/org/d3if3056/testing/model/Hewan.kt
@@ -1,7 +1,8 @@
 package org.d3if3056.testing.model
 
 data class Hewan(
-    val nama: String,
-    val namaLatin: String,
-    val imageId: String
+    val id: String,
+    val url: String,
+    val width: Int,
+    val height: Int
 )
--- app/src/main/java/org/d3if3056/testing/network/HewanApiService.kt
+++ app/src/main/java/org/d3if3056/testing/network/HewanApiService.kt
@@ -14,7 +14,7 @@ private val moshi = Moshi.Builder()
     .build()
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
