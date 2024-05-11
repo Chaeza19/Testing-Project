0382ac4f7d240da64125d5a82f0d927c688561a0
Chaeza Fauzyyah Inayah <chaeza@student.telkomuniversity.ac.id>
2024-05-11 21:23:39 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/org/d3if3056/testing/model/Hewan.kt
@@ -0,0 +1,7 @@
+package org.d3if3056.testing.model
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
--- app/src/main/java/org/d3if3056/testing/network/HewanApiService.kt
+++ app/src/main/java/org/d3if3056/testing/network/HewanApiService.kt
@@ -1,18 +1,26 @@
 package org.d3if3056.testing.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if3056.testing.model.Hewan
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://api.thecatapi.com/" + "v1/images/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
+
 interface HewanApiService {
     @GET("search?limit=10")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
